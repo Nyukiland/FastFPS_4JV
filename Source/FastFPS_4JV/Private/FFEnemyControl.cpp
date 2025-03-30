f440b3dc76d056d3be7edbafa6a0bb0d3aab7b1b
Nyukiland <roman211113@gmail.com>
2025-03-30 16:19:55 +0200

    [UPD] TASK-71: EnemyControl waves in progress

--- Source/FastFPS_4JV/Private/FFEnemyControl.cpp
+++ Source/FastFPS_4JV/Private/FFEnemyControl.cpp
@@ -8,6 +8,7 @@ AFFEnemyControl::AFFEnemyControl()
 	PrimaryActorTick.bCanEverTick = true;
 
 	EnemiesKilled = 0;
+	CurrentWave = 0;
 }
 
 AFFEnemyControl* AFFEnemyControl::GetEnemyControl(UObject* WorldContext)
@@ -36,10 +37,16 @@ AFFEnemyControl* AFFEnemyControl::GetEnemyControl(UObject* WorldContext)
 	return Instance;
 }
 
+void AFFEnemyControl::RegisterPlayer(AActor* CurPlayer)
+{
+	Player = CurPlayer;
+}
+
 void AFFEnemyControl::RegisterEnemySpawned(AActor* Spawned)
 {
 	if (!Instance) return;
 	EnemiesArray.Add(Spawned);
+	if (!Spawned->GetClass()->IsChildOf(SpawnerClass)) EnemySpawned++;
 }
 
 void AFFEnemyControl::EnemyDied(AActor* Died)
@@ -73,17 +80,108 @@ TArray<AActor*> AFFEnemyControl::GetAllEnemies()
 	return Instance ? EnemiesArray : TArray<AActor*>();
 }
 
+FWaveSetUp AFFEnemyControl::GetCurrentWaveInfo()
+{
+	if (CurrentWave >= Waves.Num()) return FWaveSetUp();
+	return Waves[CurrentWave];
+}
+
 void AFFEnemyControl::BeginPlay()
 {
 	Super::BeginPlay();
 	if (!Instance) Instance = this;
-	else this->Destroy();
+	else
+	{
+		this->Destroy();
+		return;
+	}
+
+	GetWorld()->GetTimerManager().SetTimerForNextTick(this, &AFFEnemyControl::DelayedBegin);
+}
+
+void AFFEnemyControl::DelayedBegin()
+{
+	if (Waves.Num() == 0) return;
+
+	for (int i = EnemiesArray.Num() - 1; i >= 0; --i)
+	{
+		if (EnemiesArray[i] && EnemiesArray[i]->GetClass()->IsChildOf(SpawnerClass))
+		{
+			SpawnerPos.Add(EnemiesArray[i]->GetActorLocation());
+
+			AActor* EnemyToDestroy = EnemiesArray[i];
+			EnemiesArray.RemoveAt(i);
+
+			if (EnemyToDestroy) EnemyToDestroy->Destroy();
+		}
+	}
+
+	PlaceSpawner(0);
+}
+
+void AFFEnemyControl::PlaceSpawner(int WaveCount)
+{
+	if (!Player) return;
+
+	SpawnerPos.Sort([this](const FVector& A, const FVector& B)
+		{
+			FVector PlayerPos = Player->GetActorLocation();
+			return FVector::Dist(A, PlayerPos) > FVector::Dist(B, PlayerPos);
+		});
+
+	for (int i = 0; i < Waves[WaveCount].SpawnerCount; i++)
+	{
+		AActor* Spawner = GetWorld()->SpawnActor<AActor>(SpawnerClass);
+		Spawner->SetActorLocation(SpawnerPos[i]);
+	}
 }
 
 void AFFEnemyControl::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
+	WaveControlPassive(DeltaTime);
+	WaveControlAttack(DeltaTime);
+}
+
+void AFFEnemyControl::WaveControlPassive(float DeltaTime)
+{
+	if (WaveState != EWAVESTATE::InBetween) return;
+
+	Timer += DeltaTime;
+
+	if (Timer > TimeBetweenWave)
+	{
+		WaveState = EWAVESTATE::InWave;
+		Timer = 0;
+
+		PlaceSpawner(CurrentWave);
+	}
+}
+
+void AFFEnemyControl::WaveControlAttack(float DeltaTime)
+{
+	if (WaveState != EWAVESTATE::InWave) return;
+
+	Timer += DeltaTime;
+
+	if (Timer < 0.2f) return; //wait a little for enemies to spawn
+
+	if (EnemiesArray.IsEmpty())
+	{
+		CurrentWave++;
+		if (CurrentWave >= Waves.Num())
+		{
+			WaveState = EWAVESTATE::Win;
+			return;
+		}
+
+		EnemySpawned = 0;
+
+		TimerEachWave.Add(Timer);
+		Timer = 0;
+		WaveState = EWAVESTATE::InBetween;
+	}
 }
 
 void AFFEnemyControl::EndPlay(const EEndPlayReason::Type EndPlayReason)
--- Source/FastFPS_4JV/Public/FFEnemyControl.h
+++ Source/FastFPS_4JV/Public/FFEnemyControl.h
@@ -22,6 +22,9 @@ public:
 	int EnemyCount;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EnemyManager")
+	int FlyingOnSpawner;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EnemyManager")
 	int SpawnerCount;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EnemyManager")
@@ -29,6 +32,15 @@ public:
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EnemyManager")
 	float TimeSpawn;
+};
+
+UENUM(BlueprintType)
+enum class EWAVESTATE : uint8
+{
+	InBetween,
+	InWave,
+	Win,
+	None
 };
 
 UCLASS()
@@ -43,17 +55,41 @@ public:
 public:
 	static AFFEnemyControl* Instance;
 
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "EnemyManager")
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "EnemyManagerDebug")
+	AActor* Player;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "EnemyManagerDebug")
 	TArray<AActor*> EnemiesArray;
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "EnemyManager")
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "EnemyManagerDebug")
 	int EnemiesKilled;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "EnemyManagerDebug")
+	float Timer;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "EnemyManagerDebug")
+	EWAVESTATE WaveState;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "EnemyManagerDebug")
+	TArray<float> TimerEachWave;
 
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EnemyManager")
 	TArray<FWaveSetUp> Waves;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EnemyManager")
+	float TimeBetweenWave;
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "EnemyManagerDebug")
+	TArray<FVector> SpawnerPos;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "EnemyManagerDebug")
+	int CurrentWave;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "EnemyManagerDebug")
+	int EnemySpawned;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EnemyManager")
+	TSubclassOf<AActor> SpawnerClass;
 
 public:
 	UFUNCTION(BlueprintCallable, Category = "EnemyManager")
 	static AFFEnemyControl* GetEnemyControl(UObject* WorldContext);
+
+	UFUNCTION(BlueprintCallable, Category = "EnemyManager")
+	void RegisterPlayer(AActor* CurPlayer);
 
 	UFUNCTION(BlueprintCallable, Category = "EnemyManager")
 	void RegisterEnemySpawned(AActor* Spawned);
@@ -67,8 +103,16 @@ public:
 	UFUNCTION(BlueprintCallable, Category = "EnemyManager")
 	TArray<AActor*> GetAllEnemies();
 
+	UFUNCTION(BlueprintCallable, Category = "EnemyManager")
+	FWaveSetUp GetCurrentWaveInfo();
+
 protected:	
 	virtual void BeginPlay() override;
 	virtual void Tick(float DeltaTime) override;
 	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
+	void DelayedBegin();
+	void PlaceSpawner(int WaveCount);
+	void WaveControlPassive(float DeltaTime);
+	void WaveControlAttack(float DeltaTime);
 };
